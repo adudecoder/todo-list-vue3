089b90280af744e0db2cc68e8945741462417629
Victor Martins <vitorchiesa74@gmail.com>
2022-12-03 17:13:51 -0300

    commit init

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- Todo items -->
+    <div class="space-y-2">
+        
+        <TodoItem v-for="todo in $store.state.todos" :key="todo.id" :todo="todo" />
+        
+    </div>
+    <!--/ Todo items -->
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    components: {
+        TodoItem
+    }
+}
+</script>
